fa83d68e4d5cfa50d3292b61c7c6256b3cb48d5b
Captainmorgan37 <captainmorgan37@gmail.com>
2025-10-22 12:21:06 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import reprlib
+st.caption(f"Debug (remove later): secret value = {reprlib.repr(st.secrets.get('app_password'))}")
 
 # --- Basic single-password login gate ---
 def password_gate():
