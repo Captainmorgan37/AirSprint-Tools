afacda9c851efee40b79d0df1547578c30103ab8
Captainmorgan37 <captainmorgan37@gmail.com>
2025-11-01 00:28:38 -0600

    Initialize neg_scheduler module with imports

--- core/neg_scheduler/__init__.py
+++ core/neg_scheduler/__init__.py
@@ -1,1 +1,9 @@
+# core/__init__.py
+# (can be empty)
+
+# core/neg_scheduler/__init__.py
+from .contracts import Flight, Tail
+from .model import NegotiationScheduler, LeverPolicy
+
+__all__ = ["Flight", "Tail", "NegotiationScheduler", "LeverPolicy"]
 
